a3d618c6a4d013e46649be9806d03d88d2b7fa6a
Patrick O'Sullivan <patrick@osullivan.io>
2023-08-04 15:47:25 -0500

    Fix vite config issue

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,23 +1,31 @@
-import { defineConfig } from 'vite'
-import { urbitPlugin } from '@urbit/vite-plugin-urbit'
-import react from '@vitejs/plugin-react'
+import { loadEnv, defineConfig } from 'vite';
+import { urbitPlugin } from '@urbit/vite-plugin-urbit';
+import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd()));
 
   const SHIP_URL =
     process.env.SHIP_URL ||
     process.env.VITE_SHIP_URL ||
     'http://localhost:8080';
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), urbitPlugin({
-    base: 'trackur',
-    target: SHIP_URL,
-    changeOrigin: true,
-    secure: false,
-  })],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+
+  return {
+    plugins: [
+      react(),
+      urbitPlugin({
+        base: 'trackur',
+        target: SHIP_URL,
+        changeOrigin: true,
+        secure: false,
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-})
+  };
+});
