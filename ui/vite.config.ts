73f97a6f58480b6c978f0fd7c44e153505734f56
Patrick O'Sullivan <patrick@osullivan.io>
2023-09-04 15:26:02 -0500

    Add service worker + app icons

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,6 +2,7 @@ import { loadEnv, defineConfig } from 'vite';
 import { urbitPlugin } from '@urbit/vite-plugin-urbit';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -20,6 +21,29 @@ export default defineConfig(({ mode }) => {
         target: SHIP_URL,
         changeOrigin: true,
         secure: false,
+      }),
+      VitePWA({
+        base: '/apps/trackur/',
+        injectRegister: 'inline',
+        srcDir: 'src',
+        manifest: {
+          name: 'Trackur',
+          short_name: 'Trackur',
+          description: 'Trackur',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
       }),
     ],
     resolve: {
