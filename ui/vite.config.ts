2e30c71209cade2a4a240761fca56e62d1952b4b
Patrick O'Sullivan <patrick@osullivan.io>
2023-09-05 09:53:10 -0500

    Update app description

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig(({ mode }) => {
         manifest: {
           name: 'Trackur',
           short_name: 'Trackur',
-          description: 'Trackur',
+          description: 'Track your weight, fasts, and other daily health routines (coming soon).',
           theme_color: '#ffffff',
           icons: [
             {
